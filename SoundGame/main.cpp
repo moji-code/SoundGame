a891feaca9a95e1edbfde6bd190ed3b860223917
mojito <ikuo@e.rusk.to>
2020-06-22 22:58:36 +0900

    Initial Commit

---
+++ SoundGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SoundGame
+//
+//  Created by mojito on 2020/06/22.
+//  Copyright © 2020 mojito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
